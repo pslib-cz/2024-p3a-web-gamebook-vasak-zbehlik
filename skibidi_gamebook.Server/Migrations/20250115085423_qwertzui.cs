f06abf8d1fd619d32dd852214399774f0d5cf7d5
Josef Zběhlík <josef.zbehlik.022@pslib.cz>
2025-01-15 09:55:23 +0100

    rfgb

---
+++ skibidi_gamebook.Server/Migrations/20250115085423_qwertzui.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace skibidi_gamebook.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class qwertzui : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    RoomId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Img = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.RoomId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Characters",
+                columns: table => new
+                {
+                    CharacterId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Text = table.Column<string>(type: "TEXT", nullable: false),
+                    img = table.Column<string>(type: "TEXT", nullable: false),
+                    whereId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Characters", x => x.CharacterId);
+                    table.ForeignKey(
+                        name: "FK_Characters_Rooms_whereId",
+                        column: x => x.whereId,
+                        principalTable: "Rooms",
+                        principalColumn: "RoomId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Items",
+                columns: table => new
+                {
+                    ItemId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    Img = table.Column<string>(type: "TEXT", nullable: false),
+                    RoomId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Items", x => x.ItemId);
+                    table.ForeignKey(
+                        name: "FK_Items_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "RoomId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Connections",
+                columns: table => new
+                {
+                    ConnectionId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Lock = table.Column<int>(type: "INTEGER", nullable: false),
+                    RequieremenId = table.Column<int>(type: "INTEGER", nullable: true),
+                    FromId = table.Column<int>(type: "INTEGER", nullable: true),
+                    ToId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Connections", x => x.ConnectionId);
+                    table.ForeignKey(
+                        name: "FK_Connections_Items_RequieremenId",
+                        column: x => x.RequieremenId,
+                        principalTable: "Items",
+                        principalColumn: "ItemId");
+                    table.ForeignKey(
+                        name: "FK_Connections_Rooms_FromId",
+                        column: x => x.FromId,
+                        principalTable: "Rooms",
+                        principalColumn: "RoomId");
+                    table.ForeignKey(
+                        name: "FK_Connections_Rooms_ToId",
+                        column: x => x.ToId,
+                        principalTable: "Rooms",
+                        principalColumn: "RoomId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_FromId",
+                table: "Connections",
+                column: "FromId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_RequieremenId",
+                table: "Connections",
+                column: "RequieremenId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Connections_ToId",
+                table: "Connections",
+                column: "ToId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Characters_whereId",
+                table: "Characters",
+                column: "whereId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Items_RoomId",
+                table: "Items",
+                column: "RoomId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Connections");
+
+            migrationBuilder.DropTable(
+                name: "Characters");
+
+            migrationBuilder.DropTable(
+                name: "Items");
+
+            migrationBuilder.DropTable(
+                name: "Rooms");
+        }
+    }
+}
